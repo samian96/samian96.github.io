ccaa11c3b3326301e057b9daeb17ccbc69c2de5b
samian <sam.iannazzo@yahoo.com>
2025-11-30 21:53:09 -0800

    set up a const variable for winning/ defined the rows, columns, diagonal possibility

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ let board = ["", "", "", "", "","","","",""] // sets the variable for the board
 let currentPlayer = "X"; // sets the variable for the player as X
 let gameActive = true; // sets the variable for the game to be active if true
 
+// handle win conditions //
+
+const winCombos = [
+    [0,1,2], [3,4,5], [6,7,8], // should target rows in the grid
+    [0,3,6], [1,4,7], [2,5,8], // should target columns in the grid
+    [0,4,8], [2,4,6], // should target the diagonals in the grid
+];
