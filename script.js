a7ccd63efd3cb6f08115d1c27048561c7b9c5ebd
samian <sam.iannazzo@yahoo.com>
2025-11-30 21:46:10 -0800

    set let variable for board player and active game

---
+++ script.js
@@ -0,0 +1,6 @@
+// Handling Variables //
+
+let board = ["", "", "", "", "","","","",""] // sets the variable for the board 
+let currentPlayer = "X"; // sets the variable for the player as X
+let gameActive = true; // sets the variable for the game to be active if true
+
